b3cb79e133f3d635af8b1b253c467f169952cabc
lnuvy <lnuvy.code@gmail.com>
2023-09-04 17:17:02 +0900

    Init: add README, env TODOS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
+// TODOS: 2 set Document title/description, swagger URL, server PORT
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  const config = new DocumentBuilder().setTitle('Title').setDescription('The is a sample REST API').build();
+
+  // cors
+  // app.enableCors();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(4000);
 }
 bootstrap();
